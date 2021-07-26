5e32ebc7558aeb882082b3c256257c7ee1a9c4d2
Devon DeJohn <ddejohn@pm.me>
2021-07-26 14:36:22 -0600

    fixes binomial test

--- test.py
+++ test.py
@@ -8,26 +8,26 @@ from inspect import getsource
 class Test(unittest.TestCase):
     def test_binomial(self):
         msg = "\n\nYour binomial pmf is not implemented correctly."
-        n, p = 12, 0.5
+        n, p = 25, 0.5
         student_binom = student_dists.binomial_pmf
-        student_solution = [student_binom(n, p, k) for k in range(1, 25)]
-        target_solution = [binom.pmf(k, n, p) for k in range(1, 25)]
+        student_solution = [student_binom(n, p, k) for k in range(n)]
+        target_solution = [binom.pmf(k, n, p) for k in range(n)]
         for s, t in zip(student_solution, target_solution):
             self.assertAlmostEqual(s, t, places=5, msg=msg)
 
     def test_poisson(self):
+        lam = 10
         msg = "\n\nYour poisson pmf is not implemented correctly."
         student_pois = student_dists.poisson_pmf
-        lam = 10
         student_solution = [student_pois(k, lam) for k in range(1, 25)]
         target_solution = [poisson.pmf(k, lam) for k in range(1, 25)]
         for s, t in zip(student_solution, target_solution):
             self.assertAlmostEqual(s, t, places=5, msg=msg)
 
     def test_geometric(self):
+        p = 0.25
         msg = "\n\nYour geometric pmf is not implemented correctly."
         student_geom = student_dists.geometric_pmf
-        p = 0.25
         student_solution = [student_geom(k, p) for k in range(1, 25)]
         target_solution = [geom.pmf(k, p) for k in range(1, 25)]
         for s, t in zip(student_solution, target_solution):
